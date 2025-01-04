6a4226b9ec7346c53d1752ee5239f0f779fe5e0a
oelsheha <oelsheha@sfu.ca>
2025-01-04 00:15:40 -0800

    fix: added auth protection for middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,14 @@
-import { clerkMiddleware} from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-// const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
 
-export default clerkMiddleware();
+export default clerkMiddleware(
+  async (auth, request) => {
+    if (!isPublicRoute(request)) {
+      await auth.protect();
+    }
+  },
+);
 
 export const config = {
   matcher: [
